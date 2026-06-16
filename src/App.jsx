e3870257042609dfe686ee160bb161808d8d0588
Fede301 <federicofof305@gmail.com>
2026-06-16 16:35:10 +0200

    Base

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { Button } from 'react-bootstrap'
+
+function App() {
+
+  return (
+    <>
+      <h1>Texto cualquiera</h1>
+    </>
+  )
+}
+
+export default App
