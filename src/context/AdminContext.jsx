21c88f027131784d3c00c34d880e5216cc5e80d9
micaelsoruco332 <micaelsoruco332@gmail.com>
2026-06-22 01:16:04 -0300

    AdminContext con persistencia localstorage

--- src/context/AdminContext.jsx
+++ src/context/AdminContext.jsx
@@ -1,17 +1,14 @@
 import { createContext, useState, useEffect } from "react";
 
-// 1. Creamos y exportamos el contexto
 export const AdminContext = createContext();
 
 export const AdminProvider = ({ children }) => {
 
-    // 2. Estado inicial leyendo LocalStorage (vuelve a null si no hay datos) 
     const [admin, setAdmin] = useState(() => {
         const savedAdmin = localStorage.getItem("adminSession");
         return savedAdmin ? JSON.parse(savedAdmin) : null;
     });
 
-    // 3. Persistencia automática con LocalStorage 
     useEffect(() => {
         if (admin) {
             localStorage.setItem("adminSession", JSON.stringify(admin));
@@ -20,12 +17,10 @@ export const AdminProvider = ({ children }) => {
         }
     }, [admin]);
 
-    // 4. Función para iniciar sesión 
     const login = (nombre, sector) => {
         setAdmin({ nombre, sector });
     };
 
-    // 5. Función para cerrar sesión 
     const logout = () => {
         setAdmin(null);
     };
